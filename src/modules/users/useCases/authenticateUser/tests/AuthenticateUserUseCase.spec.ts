983ef320ca5d86af0c0de020f1c820a6cc6f3684
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-08-29 21:58:57 -0300

    Authentication tests finalized

--- src/modules/users/useCases/authenticateUser/tests/AuthenticateUserUseCase.spec.ts
+++ src/modules/users/useCases/authenticateUser/tests/AuthenticateUserUseCase.spec.ts
@@ -1,6 +1,7 @@
 import { InMemoryUsersRepository } from "../../../repositories/in-memory/InMemoryUsersRepository";
 import { CreateUserUseCase } from "../../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "../AuthenticateUserUseCase";
+import { IncorrectEmailOrPasswordError } from "../IncorrectEmailOrPasswordError";
 
 
 let inMemoryUserRepository: InMemoryUsersRepository;
@@ -30,5 +31,40 @@ describe("Authenticate User", () => {
     });
 
     expect(userAuthenticated).toHaveProperty("token");
+  });
+
+  it("Should not be able to authenticate a user with invalid email", async () => {
+    const user = {
+      name: "User Test",
+      email: "antonio@dev.com",
+      password: "123123"
+    };
+
+    await createUserUseCase.execute(user);
+
+    expect(async () => {
+       await authenticateUserUseCase.execute({
+        email: "other_email@test.com",
+        password: user.password
+      });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
+  });
+
+
+  it("Should not be able to authenticate a user with invalid password", async () => {
+    const user = {
+      name: "User Test",
+      email: "antonio@dev.com",
+      password: "123123"
+    };
+
+    await createUserUseCase.execute(user);
+
+    expect(async () => {
+       await authenticateUserUseCase.execute({
+        email: user.email,
+        password: "incorrect_password"
+      });
+    }).rejects.toBeInstanceOf(IncorrectEmailOrPasswordError)
   });
 });
