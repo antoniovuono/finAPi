ed2753003270bb4bd3f506f54ca42510a68aa9d2
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2022-09-04 20:55:19 -0300

    Statements tests finalized

--- src/modules/statements/useCases/createStatement/tests/CreateStatamentController.spec.ts
+++ src/modules/statements/useCases/createStatement/tests/CreateStatamentController.spec.ts
@@ -1,8 +1,9 @@
 import { Connection } from "typeorm";
-import request from "supertest";
-import {v4 as uuid} from "uuid";
 import createConnection from "../../../../../database";
+import { v4 as uuid } from "uuid";
 import { hash } from "bcryptjs";
+import request from "supertest";
+import { app } from "../../../../../app";
 
 let connection: Connection;
 
@@ -12,16 +13,60 @@ describe("Create Statement Controller", () => {
     await connection.runMigrations();
 
     const id = uuid();
-    const password = hash("admin", 8);
+    const password = await hash("admin", 8);
 
-    connection.query(`
+    await connection.query(`
     INSERT INTO USERS(id, name, email, password, created_at, updated_at)
       values('${id}', 'User Test', 'antonio@dev.com', '${password}', 'now()', 'now()')
-    `);
+  `);
   });
 
   afterAll(async () => {
     await connection.dropDatabase();
     await connection.close();
+  });
+
+  it("Should be able to create a deposit statement", async () => {
+    const userAuthenticated = await request(app).post('/api/v1/sessions')
+    .send({
+      email: "antonio@dev.com",
+      password: "admin"
+    });
+
+    const { token } = userAuthenticated.body;
+
+    const depositStatement = await request(app).post('/api/v1/statements/deposit')
+    .send({
+      amount: 300,
+      description: "Test deposit"
+    })
+    .set({
+      Authorization: `Bearer ${token}`
+    });
+
+    expect(depositStatement.status).toBe(201);
+    expect(depositStatement.body).toHaveProperty("id");
+  });
+
+  it("Should be able to create a withdraw statement", async () => {
+    const userAuthenticated = await request(app).post('/api/v1/sessions')
+    .send({
+      email: "antonio@dev.com",
+      password: "admin"
+    });
+
+    const { token } = userAuthenticated.body;
+
+    const withdrawStatement = await request(app).post('/api/v1/statements/withdraw')
+    .send({
+      amount: 300,
+      description: "Test deposit"
+    })
+    .set({
+      Authorization: `Bearer ${token}`
+    });
+
+    expect(withdrawStatement.status).toBe(201);
+    expect(withdrawStatement.body).toHaveProperty("id");
   });
 });
